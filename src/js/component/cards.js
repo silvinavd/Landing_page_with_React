fb3a55d965793d7e0a6325eefbb884a384d32a36
silvinavd <silvinavalverde8@gmail.com>
2021-04-27 19:12:54 +0000

    1

---
+++ src/js/component/cards.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Cards() {
+    return (
+        <div className="card-deck">
+            <div className="card">
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>  
+            
+            </div>
+
+
+    );
+}
